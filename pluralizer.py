a4a85c7ab353bccfbe1b9801da016945add91aa6
e <71161672+KetchupChips1@users.noreply.github.com>
2020-12-21 00:58:02 -0500

    Update pluralizer.py
    It's quite brute-forcey but the English language is weird

--- pluralizer.py
+++ pluralizer.py
@@ -1,6 +1,5 @@
 def plural(str):
 #variables
-
     vows = ('a', 'e', 'i', 'o', 'u')
     cons = ('b', 'c', 'd', 'f', 'g', 'h', 'j', 'k', 'l', 'm', 'n', 'p', 'q', 'r', 's', 't', 'v', 'w', 'x', 'y', 'z')
     chars = []
